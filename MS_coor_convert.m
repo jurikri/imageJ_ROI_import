0091ccbf54f89710fc9b566e9593d308f0f6c6a2
jurikri <41930943+jurikri@users.noreply.github.com>
2019-04-01 20:06:38 +0900

    하위버전과의 호환성을 위하여, mat format대신 xls format을 사용하도록 수정

--- MS_coor_convert.m
+++ MS_coor_convert.m
@@ -1,8 +1,21 @@
 clear; clc; close all;
 
 %% select pathway
-file_nm = []; path = []; [file_nm, path] = uigetfile(fullfile(path, '*.mat')); filepath = [path, file_nm];
-load(filepath)
+file_nm = []; path = []; [file_nm, path] = uigetfile(fullfile(path, '*.xls')); filepath = [path, file_nm];
+
+%% transfer Coor
+cnt = 0;
+while 1
+    cnt = cnt+1
+    try
+        Coor{cnt,1} = xlsread(filepath, cnt);
+    catch
+        break
+    end
+end
+ 
+ijm = string(zeros((size(Coor, 1)*2)+2,1));
+
 %% make save folder, varience
 filepath = [path 'save'];
 if (exist(filepath, 'dir') == 0)
@@ -10,7 +23,6 @@ if (exist(filepath, 'dir') == 0)
     mkdir(filepath);
 end
 
-ijm = string(zeros((size(Coor, 1)*2)+2,1));
 
 %% basic setup for ijm
 tmp = 'roiManager("Show All with labels");';
@@ -25,10 +37,10 @@ filepath2 = strrep(filepath,'\','/');
 
 for ROINum = 1:size(Coor, 1)
     matrix1 = cell2mat(Coor(ROINum,1));
-    matrix2 = transpose(matrix1);
+%     matrix2 = transpose(matrix1);
     
     filename = [filepath2 '/' num2str(ROINum) '.csv'];
-    csvwrite(filename, matrix2)
+    csvwrite(filename, matrix1)
    
     tmp =  ['run("XY Coordinates... ", "open=[' filename ']");'];
     ijm(ROINum*2-1+2,1) = tmp;
@@ -36,10 +48,11 @@ for ROINum = 1:size(Coor, 1)
     ijm(ROINum*2-0+2,1) = tmpadd;
 end
 
-writematrix(ijm,[filepath '\mss.csv']);
+writematrix(ijm,[path 'imj.csv']);
 
 %%
 
+disp('done')
 
 
 
